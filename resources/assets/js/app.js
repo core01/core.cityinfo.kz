68e1efd6abb33797f21772a31dd01f74cb710abb
Roman Sadoyan <ra@sadoyan.ru>
2017-10-30 00:25:50 +0300

    Сборка frontend

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,22 +1,23 @@
-
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
-
-window.Vue = require('vue');
-
+import Vue from 'vue';
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example', require('./components/Example.vue'));
+import AuthLogout from './components/auth/logout.vue';
+import users from './components/manager/users.vue';
+import companyMeta from './components/manager/companyMeta.vue';
 
 const app = new Vue({
-    el: '#app'
+  el: '#app',
+  components: {
+    AuthLogout, users, companyMeta
+  }
 });
