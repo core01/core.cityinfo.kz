bd61242ff4ac0bf2ac74d6b58b140b4859dccf26
Roman Sadoyan <ra@sadoyan.ru>
2017-10-30 00:17:55 +0300

    Добавлен стартовый сидер для создания пользователя и ролей с правами

---
+++ database/seeds/StarterDataSeeder.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use \Illuminate\Support\Facades\Auth;
+use \App\Models\User;
+
+class StarterDataSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $user = User::role('admin')->first();
+        if (!$user) {
+            $user = new User();
+            $user->name = 'Super Admin';
+            $user->email = 'admin@example.com';
+            $user->password = bcrypt('admin');
+            $user->save();
+        }
+        Auth::login($user);
+        // Reset cached roles and permissions
+        app()['cache']->forget('spatie.permission.cache');
+
+        // create permissions
+
+        $deleteUsers = Permission::firstOrCreate(['name' => 'delete users']);
+        $manageUsers = Permission::firstOrCreate(['name' => 'manage users']);
+        $manageCm = Permission::firstOrCreate(['name' => 'manage company meta']);
+        $admin = Role::firstOrCreate(['name' => 'admin']);
+
+
+        if (!$user->hasRole($admin->name)) {
+            $user->assignRole($admin->name);
+        }
+        if (!$admin->hasPermissionTo($deleteUsers->name)) {
+            $admin->givePermissionTo($deleteUsers->name);
+        }
+        if (!$admin->hasPermissionTo($manageUsers->name)) {
+            $admin->givePermissionTo($manageUsers->name);
+        }
+        if (!$admin->hasPermissionTo($manageCm->name)) {
+            $admin->givePermissionTo($manageCm->name);
+        }
+
+
+    }
+}
