e6aeb85354785f557fe2b1a7112dbfa8fc781432
Roman Sadoyan <ra@sadoyan.ru>
2017-10-30 00:11:37 +0300

    Компонент logout

---
+++ resources/assets/js/components/auth/logout.vue
@@ -0,0 +1,67 @@
+<template>
+    <div v-on-clickaway="away" class="navbar-item has-dropdown" :class="{'is-active': active}">
+        <a class="navbar-link dropdown-trigger" @click="showDropdown">
+            {{ name }}
+        </a>
+
+        <div class="navbar-dropdown">
+            <a class="navbar-item" :href="profileUrl" :class="{'is-active': url === profileUrl }">
+                Профиль
+            </a>
+            <a class="navbar-item" @click="logout">
+                Logout
+            </a>
+        </div>
+    </div>
+</template>
+
+<script>
+  import axios from 'axios'
+  import { mixin as clickaway } from 'vue-clickaway'
+
+  export default {
+    mixins: [clickaway],
+    data () {
+      return {
+        active: false,
+        url: location.href,
+      }
+    },
+    props: ['name', 'logoutUrl', 'profileUrl'],
+    methods: {
+      showDropdown () {
+        this.active = !this.active;
+      },
+      away: function () {
+        this.active = false
+      },
+      logout: function () {
+        let token = document.querySelector('[name=csrf-token]')
+        axios.post(this.logoutUrl, {
+          _token: token.content,
+        })
+          .then(function (response) {
+            location.href = response.data.redirect
+          })
+          .catch(function (error) {
+            console.log(error)
+          })
+      },
+      isActive: (url) => {
+        return url === this.url;
+      }
+    },
+    computed: {
+
+    },
+    mounted () {
+
+    },
+    created () {
+
+    },
+    destroyed () {
+
+    }
+  }
+</script>
