d0967d3e540fbae9b6f2c28d4727793ff6ae9d00
Roman Sadoyan <ra@sadoyan.ru>
2018-01-17 15:45:56 +0600

    Remove telegram, rebuild js, update dependency

--- resources/assets/js/components/manager/companyMeta.vue
+++ resources/assets/js/components/manager/companyMeta.vue
@@ -96,168 +96,171 @@
 </template>
 
 <script>
-  import axios from 'axios';
-  import routes from '../../routes';
-  import VueUploadComponent from 'vue-upload-component';
+import axios from 'axios'
+import routes from '../../routes'
+import VueUploadComponent from 'vue-upload-component'
 
-  export default {
-    data () {
-      return {
+export default {
+  data() {
+    return {
+      company_id: '',
+      description: '',
+      saveDisabled: true,
+      errorsArray: {
         company_id: '',
-        description: '',
-        saveDisabled: true,
-        errorsArray: {
-          company_id: '',
-          description: '',
-        },
-        routes: routes,
-        images: [],
-        files: [],
-        token: document.querySelector('[name=csrf-token]').content
-      };
-    },
-    components: {
-      'file-upload': VueUploadComponent
-    },
-    props: [],
-    methods: {
-      /**
-       * Has changed
-       * @param  Object|undefined   newFile   Read only
-       * @param  Object|undefined   oldFile   Read only
-       * @return undefined
-       */
-      inputFile (newFile, oldFile) {
-        let vm = this;
-        if (newFile && oldFile && !newFile.active && oldFile.active) {
-          if (newFile.xhr.status === 200) {
-            //  Get the response status code
-            let id = oldFile.id;
-            for (let file in vm.files) {
-              if (vm.files.hasOwnProperty(file)) {
-                if (vm.files[file].id === id) {
-                  vm.images.push({'src': newFile.response});
-                  vm.files.splice(file, 1);
-                }
+        description: ''
+      },
+      routes: routes,
+      images: [],
+      files: [],
+      token: document.querySelector('[name=csrf-token]').content
+    }
+  },
+  components: {
+    'file-upload': VueUploadComponent
+  },
+  props: [],
+  methods: {
+    /**
+     * Has changed
+     * @param  Object|undefined   newFile   Read only
+     * @param  Object|undefined   oldFile   Read only
+     * @return undefined
+     */
+    inputFile(newFile, oldFile) {
+      let vm = this
+      if (newFile && oldFile && !newFile.active && oldFile.active) {
+        if (newFile.xhr.status === 200) {
+          //  Get the response status code
+          let id = oldFile.id
+          for (let file in vm.files) {
+            if (vm.files.hasOwnProperty(file)) {
+              if (vm.files[file].id === id) {
+                vm.images.push({ src: newFile.response })
+                vm.files.splice(file, 1)
               }
             }
-
-            //console.log('status', newFile.xhr);
           }
-        }
-      },
-      /**
-       * Pretreatment
-       * @param  Object|undefined   newFile   Read and write
-       * @param  Object|undefined   oldFile   Read only
-       * @param  Function           prevent   Prevent changing
-       * @return undefined
-       */
-      inputFilter: function (newFile, oldFile, prevent) {
-        if (newFile && !oldFile) {
-          // Filter non-image file
-          if (!/\.(jpeg|jpe|jpg|gif|png|webp)$/i.test(newFile.name)) {
-            return prevent();
-          }
-        }
 
-        // Create a blob field
-        newFile.blob = '';
-        let URL = window.URL || window.webkitURL;
-        if (URL && URL.createObjectURL) {
-          newFile.blob = URL.createObjectURL(newFile.file);
-        }
-      },
-
-      save () {
-        let vm = this;
-        axios.post(routes.route('manager.save.company.meta'), {
-          company_id: this.company_id,
-          description: this.description,
-          images: this.images,
-        }).then(function (response) {
-          if (response.data.success) {
-            vm.$notify.success('Данные успешно сохранены');
-          } else {
-            vm.$notify.success('Ошибка при сохранении, обратитесь к администратору');
-            console.log(response.data);
-          }
-          vm.company_id = '';
-          vm.description = '';
-          vm.saveDisabled = true;
-          vm.images = [];
-        })
-          .catch(function (error) {
-            let errors = error.response.data.errors;
-            if (errors) {
-              for (let prop in errors) {
-                vm.errorsArray[prop] = errors[prop][0];
-              }
-            }
-          });
-      },
-      clean () {
-        this.company_id = '';
-        this.description = '';
-        this.images = [];
-        this.saveDisabled = true;
-      },
-      findCompany () {
-        let vm = this;
-        vm.description = '';
-        axios.get(routes.route('manager.get.company.meta'), {
-          params: {
-            company_id: this.company_id
-          }
-        }).then(function (response) {
-          if (response.data.success) {
-            vm.saveDisabled = false;
-
-            if (response.data.data) {
-              vm.$notify.info('Данные о компании загружены!');
-              vm.description = response.data.data.description;
-              if (response.data.data.images !== null) {
-                vm.images = JSON.parse(response.data.data.images);
-              }
-            } else {
-              vm.$notify.info('Данных о компании нет, создайте их!');
-            }
-          }
-        })
-          .catch(function (error) {
-            console.log(error);
-          });
-      },
-      deleteImage (index, uploaded = false) {
-        if (uploaded === false) {
-          this.files.splice(index, 1)
-        } else {
-          let vm = this
-          axios.delete(this.routes.route('image.delete'), {
-            params: vm.images[index]
-
-          }).then(function (response) {
-            vm.images.splice(index, 1)
-            vm.$notify.success('Изображение удалено!')
-          })
-            .catch(function (error) {
-              console.log(error.response)
-              vm.$notify.danger('Ошибка при удалении изображения')
-            })
+          //console.log('status', newFile.xhr);
         }
       }
     },
-    computed: {},
-    mounted () {
+    /**
+     * Pretreatment
+     * @param  Object|undefined   newFile   Read and write
+     * @param  Object|undefined   oldFile   Read only
+     * @param  Function           prevent   Prevent changing
+     * @return undefined
+     */
+    inputFilter: function(newFile, oldFile, prevent) {
+      if (newFile && !oldFile) {
+        // Filter non-image file
+        if (!/\.(jpeg|jpe|jpg|gif|png|webp)$/i.test(newFile.name)) {
+          return prevent()
+        }
+      }
 
+      // Create a blob field
+      newFile.blob = ''
+      let URL = window.URL || window.webkitURL
+      if (URL && URL.createObjectURL) {
+        newFile.blob = URL.createObjectURL(newFile.file)
+      }
     },
-    created () {
 
+    save() {
+      let vm = this
+      axios
+        .post(routes.route('manager.save.company.meta'), {
+          company_id: this.company_id,
+          description: this.description,
+          images: this.images
+        })
+        .then(function(response) {
+          if (response.data.success) {
+            vm.$notify.success('Данные успешно сохранены')
+          } else {
+            vm.$notify.success(
+              'Ошибка при сохранении, обратитесь к администратору'
+            )
+            console.log(response.data)
+          }
+          vm.company_id = ''
+          vm.description = ''
+          vm.saveDisabled = true
+          vm.images = []
+        })
+        .catch(function(error) {
+          let errors = error.response.data.errors
+          if (errors) {
+            for (let prop in errors) {
+              vm.errorsArray[prop] = errors[prop][0]
+            }
+          }
+        })
     },
-    destroyed () {
+    clean() {
+      this.company_id = ''
+      this.description = ''
+      this.images = []
+      this.saveDisabled = true
+    },
+    findCompany() {
+      let vm = this
+      vm.description = ''
+      axios
+        .get(routes.route('manager.get.company.meta'), {
+          params: {
+            company_id: this.company_id
+          }
+        })
+        .then(function(response) {
+          if (response.data.success) {
+            vm.saveDisabled = false
 
+            if (response.data.data) {
+              vm.$notify.info('Данные о компании загружены!')
+              vm.description = response.data.data.description
+              if (response.data.data.images !== null) {
+                vm.images = JSON.parse(response.data.data.images)
+              }
+            } else {
+              vm.$notify.info('Данных о компании нет, создайте их!')
+              vm.images = []
+              vm.files = []
+            }
+          }
+        })
+        .catch(function(error) {
+          console.log(error)
+        })
+    },
+    deleteImage(index, uploaded = false) {
+      if (uploaded === false) {
+        this.files.splice(index, 1)
+      } else {
+        let vm = this
+        axios
+          .delete(this.routes.route('image.delete'), {
+            params: vm.images[index]
+          })
+          .then(function(response) {
+            vm.images.splice(index, 1)
+            vm.$notify.success('Изображение удалено!')
+          })
+          .catch(function(error) {
+            console.log(error.response)
+            vm.$notify.danger('Ошибка при удалении изображения')
+          })
+      }
     }
-  };
+  },
+  computed: {},
+  mounted() {},
+  created() {},
+  destroyed() {}
+}
 </script>
 
 <style lang="sass" scoped>
